0082907736a72250e69c16d4b4e2ab97ffbee9de
bharathappali <bharath.appali@gmail.com>
2022-06-25 14:23:33 +0530

    Adds limited threshold and only 1 cpu used per container
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- clean_up.sh
+++ clean_up.sh
@@ -3,8 +3,8 @@
 storj_identies_home="${HOME}/storj-identies/*"
 for identity_folder in ${storj_identies_home}; do
 if [ -z "$(ls -A ${identity_folder})" ]; then
-   echo -n "${identity_folder} found to be empty. Removing ... "
-   rm -rf ${identity_folder}
-   echo "Done."
+    echo -n "${identity_folder} found to be empty. Removing ... "
+    rm -rf ${identity_folder}
+    echo "Done."
 fi
 done
--- run_generator.sh
+++ run_generator.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
 
+re='^[0-9]+$'
+if [ "$#" -gt 2 ]; then
+    echo "ERROR: Too many params. Only threshold, cpus is accepted. \n Exiting Gracefully." >&2; exit 1
+fi
+
+if [ "$#" -lt 1 ]; then
+    echo "ERROR: No params passed. Needs threshold value to start. \n Exiting Gracefully." >&2; exit 1
+fi
+
+THRESHOLD=${1}
+if ! [[ ${THRESHOLD} =~ $re ]] ; then
+    echo "ERROR: Invalid Threshold. \n Exiting Gracefully." >&2; exit 1
+fi
+
+echo "Threshold: ${THRESHOLD}"
+
+CPUS=0
+if [ "$#" -ne 2 ]; then
+    if ! [ -x "$(command -v nproc)" ]; then
+        echo "WARN: nproc not found using /proc/cpuinfo for getting cpu count"
+        CPUS=$(grep -c ^processor /proc/cpuinfo)
+    else
+        CPUS=$(nproc --all)
+    fi
+else
+    CPUS=${2}
+    if ! [[ ${CPUS} =~ $re ]] ; then
+        echo "ERROR: Invalid CPU value. Resetting to machine CPU's."
+        if ! [ -x "$(command -v nproc)" ]; then
+            echo "WARN: nproc not found using /proc/cpuinfo for getting cpu count"
+            CPUS=$(grep -c ^processor /proc/cpuinfo)
+        else
+            CPUS=$(nproc --all)
+        fi
+    fi
+fi
+
+echo "CPUS: ${CPUS}"
+
+if [ "${THRESHOLD}" -gt "${CPUS}" ]; then
+    echo "Threshold is greater than CPUS. Resetting threshold to ${CPUS}"
+    THRESHOLD=${CPUS}
+fi
+
 if ! [ -x "$(command -v docker)" ]; then
   echo "Please install docker to run the script. Exiting."
   exit 1
 fi
-mkdir -p "${HOME}/storj-identies
+
+mkdir -p "${HOME}/storj-identies"
 
 storj_identies_home="${HOME}/storj-identies/*"
 for identity_folder in ${storj_identies_home}; do
@@ -21,7 +66,7 @@ for (( count=1;count<=${THRESHOLD};count++ ))
 do
     mkdir -p "${HOME}/storj-identies/${random}"
     dir_to_mount="${HOME}/storj-identies/${random}"
-    docker run --rm -d --name storj-identity-generator-${count} -v ${dir_to_mount}:/app/identity -it bharathappali/storj-identity-generator:latest
+    docker run --rm -d --cpus=1 --name storj-identity-generator-${count} -v ${dir_to_mount}:/app/identity -it bharathappali/storj-identity-generator:latest
     random=$(echo $RANDOM | md5sum | head -c 20)
 done
 for (( ; ; ))
@@ -36,7 +81,7 @@ do
             random=$(echo $RANDOM | md5sum | head -c 20)
             mkdir -p "${HOME}/storj-identies/${random}"
             dir_to_mount="${HOME}/storj-identies/${random}"
-            docker run --rm -d --name storj-identity-generator-${count} -v ${dir_to_mount}:/app/identity -it bharathappali/storj-identity-generator:latest
+            docker run --rm -d --cpus=1 --name storj-identity-generator-${count} -v ${dir_to_mount}:/app/identity -it bharathappali/storj-identity-generator:latest
         fi
     done
     echo -n "Sleeping for 60 secs ... "
